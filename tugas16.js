88c9243300f8d0243b5c816ead4c25e0950fe4e7
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:31:02 +0700

    Tugas 16 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas16.js
@@ -0,0 +1,19 @@
+function panggilAku() {
+  var min = Math.min(45,100,99,3,2,4,1,2,3,19,22,14,10);
+  console.log("1. Deret angka pertama = 45,100,99,3,2,4,1,2,3,19,22,14,10");
+  console.log("Nilai Minimal : " + min);
+  var max = Math.max(45,100,99,3,2,4,1,2,3,19,22,14,10);
+  console.log("Nilai Maksimal : " + max);
+  console.log("====================");
+}
+
+function panggilAku2() {
+  var min = Math.min(45,100,99,3,2,81,22,55,1,2,3,4,5);
+  console.log("2. Deret angka kedua = 45,100,99,3,2,81,22,55,1,2,3,4,5");
+  console.log("Nilai Manimal : " + min);
+  var max = Math.max(45,100,99,3,2,81,22,55,1,2,3,4,5);
+  console.log("Nilai Maksimal : " + max);
+}
+
+panggilAku()
+panggilAku2()
